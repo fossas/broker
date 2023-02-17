b84242308f932afc3f607ac834fdb17c37ea5cea
Jessica Black <me@jessica.black>
2023-02-17 11:59:59 -0800

    Color help and context

--- src/ext/error_stack.rs
+++ src/ext/error_stack.rs
@@ -1,5 +1,6 @@
 //! Extensions to `error_stack`.
 
+use colored::Colorize;
 use error_stack::ResultExt;
 
 /// Used to provide help text to an error.
@@ -20,24 +21,31 @@ pub trait ErrorHelper {
 
 impl<T, C> ErrorHelper for error_stack::Result<T, C> {
     fn help<S: AsRef<str>>(self, help_text: S) -> Self {
+        let help = help_literal();
         let help_text = help_text.as_ref();
-        self.attach_printable_lazy(|| format!("help: {help_text}"))
+        self.attach_printable_lazy(|| format!("{help} {help_text}"))
     }
 
     fn help_if<S: AsRef<str>>(self, should_help: bool, help_text: S) -> Self {
         if should_help {
+            let help = help_literal();
             let help_text = help_text.as_ref();
-            self.attach_printable_lazy(|| format!("help: {help_text}"))
+            self.attach_printable_lazy(|| format!("{help} {help_text}"))
         } else {
             self
         }
     }
 
     fn help_lazy<S: AsRef<str>, F: FnOnce() -> S>(self, helper: F) -> Self {
+        let help = help_literal();
         let help_text = helper();
         let help_text = help_text.as_ref();
-        self.attach_printable_lazy(|| format!("help: {help_text}"))
+        self.attach_printable_lazy(|| format!("{help} {help_text}"))
     }
+}
+
+fn help_literal() -> String {
+    "help:".bold().blue().to_string()
 }
 
 /// Used to provide a description of the operation being performed when an error occurred.
@@ -54,22 +62,29 @@ pub trait DescribeContext {
 
 impl<T, C> DescribeContext for error_stack::Result<T, C> {
     fn describe<S: AsRef<str>>(self, description: S) -> Self {
+        let context = describe_literal();
         let description = description.as_ref();
-        self.attach_printable_lazy(|| format!("context: {description}"))
+        self.attach_printable_lazy(|| format!("{context} {description}"))
     }
 
     fn describe_if<S: AsRef<str>>(self, should_describe: bool, description: S) -> Self {
         if should_describe {
+            let context = describe_literal();
             let description = description.as_ref();
-            self.attach_printable_lazy(|| format!("context: {description}"))
+            self.attach_printable_lazy(|| format!("{context} {description}"))
         } else {
             self
         }
     }
 
     fn describe_lazy<S: AsRef<str>, F: FnOnce() -> S>(self, describer: F) -> Self {
+        let context = describe_literal();
         let description = describer();
         let description = description.as_ref();
-        self.attach_printable_lazy(|| format!("context: {description}"))
+        self.attach_printable_lazy(|| format!("{context} {description}"))
     }
+}
+
+fn describe_literal() -> String {
+    "context:".bold().green().to_string()
 }
