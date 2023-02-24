59408413512406422f7e9e381e3632dc988df93c
Jessica Black <me@jessica.black>
2023-02-24 15:56:35 -0800

    Validate debugging artifact max size

--- tests/it/debug.rs
+++ tests/it/debug.rs
@@ -1,6 +1,7 @@
 //! Tests for debugging functionality.
 
-use broker::debug::{ArtifactMaxAge, MIN_RETENTION_AGE};
+use broker::debug::{ArtifactMaxAge, ArtifactMaxSize, MIN_RETENTION_AGE, MIN_RETENTION_SIZE_BYTES};
+use bytesize::ByteSize;
 use proptest::prelude::*;
 use test_strategy::proptest;
 
@@ -42,5 +43,33 @@ fn validate_artifact_max_age_below_min() {
     assert_error_stack_snapshot!(
         &input,
         ArtifactMaxAge::try_from(input).expect_err("must have failed validation")
+    )
+}
+
+#[proptest]
+fn validate_artifact_max_size(
+    #[filter(#user_input as u64 > MIN_RETENTION_SIZE_BYTES)] user_input: u16,
+) {
+    let user_input = user_input as u64;
+    match ArtifactMaxSize::try_from(user_input) {
+        Ok(validated) => prop_assert_eq!(
+            validated.as_ref(),
+            &ByteSize::b(user_input),
+            "tested input: {:?}",
+            input
+        ),
+        Err(err) => prop_assert!(
+            false,
+            "unexpected parsing error '{err:#}' for input '{user_input}'"
+        ),
+    }
+}
+
+#[test]
+fn validate_artifact_max_size_below_min() {
+    let input = 100;
+    assert_error_stack_snapshot!(
+        &input,
+        ArtifactMaxSize::try_from(input).expect_err("must have failed validation")
     )
 }
