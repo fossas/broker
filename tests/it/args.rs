7d842a3f3aacd106f11b8e927c190d81cda1611f
Jessica Black <me@jessica.black>
2023-02-24 17:07:12 -0800

    Proptest & snapshot fossa args too

--- tests/it/args.rs
+++ tests/it/args.rs
@@ -1,8 +1,15 @@
 use std::path::PathBuf;
 
-use broker::config;
+use broker::{
+    api::fossa::{Endpoint, Key},
+    config,
+};
+use secrecy::ExposeSecret;
+use url::Url;
 
 use crate::helper::assert_error_stack_snapshot;
+use proptest::prelude::*;
+use test_strategy::proptest;
 
 pub fn raw_base_args(config: &str, db: &str) -> config::RawBaseArgs {
     config::RawBaseArgs::new(Some(String::from(config)), Some(String::from(db)))
@@ -64,4 +71,60 @@ fn errors_on_nonexistent_both() {
         &base,
         config::validate_args(base).expect_err("args must have failed validation")
     );
+}
+
+#[proptest]
+fn fossa_api_endpoint(#[strategy(r#"\PC+"#)] user_input: String) {
+    let canonical = Url::parse(&user_input);
+    let validated = Endpoint::try_from(user_input.clone());
+
+    match (canonical, validated) {
+        (Ok(canonical), Ok(validated)) => prop_assert_eq!(
+            validated.as_ref(),
+            &canonical,
+            "parsed URLs must be equal parsing input {}",
+            user_input
+        ),
+        (Err(canonical), Err(validated)) => {
+            let contains = format!("{validated:#}").contains(&canonical.to_string());
+            prop_assert!(
+                contains,
+                "validation error must contain parser error, parsing input {}",
+                user_input
+            );
+        }
+        (Ok(canonical), Err(validated)) => {
+            prop_assert_eq!(
+                format!("{validated:#}"),
+                canonical.as_str(),
+                "parser and validator must not disagree parsing input {}",
+                user_input
+            )
+        }
+        (Err(canonical), Ok(validated)) => {
+            prop_assert_eq!(
+                validated.as_ref().as_str(),
+                format!("{canonical:#}"),
+                "parser and validator must not disagree parsing input {}",
+                user_input
+            )
+        }
+    }
+}
+
+#[proptest]
+fn fossa_api_key(#[strategy(r#"\PC+"#)] user_input: String) {
+    match Key::try_from(user_input.clone()) {
+        Ok(validated) => prop_assert_eq!(validated.as_ref().as_ref().expose_secret(), &user_input),
+        Err(err) => prop_assert!(false, "unexpected parse error: {:#}", err),
+    }
+}
+
+#[test]
+fn fossa_api_key_empty() {
+    let input = String::from("");
+    assert_error_stack_snapshot!(
+        &input,
+        Key::try_from(input).expect_err("must have failed validation")
+    )
 }
