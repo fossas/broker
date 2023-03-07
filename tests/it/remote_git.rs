840a5b578f47179b13119c2de440a9a336c3cf79
spatten <scott@fossa.com>
2023-03-07 13:22:17 -0800

    add tests for update_clones

--- tests/it/remote_git.rs
+++ tests/it/remote_git.rs
@@ -1,57 +1,47 @@
 //! Tests for git remotes
 
-use crate::helper::assert_error_stack_snapshot;
-use std::{
-    fs::{self},
-    path::PathBuf,
-};
+use crate::{args::raw_base_args, helper::load_config};
+use broker::api::remote::RemoteProvider;
+use std::path::PathBuf;
+
 use tempfile::tempdir;
 
-use broker::api::remote::{self, git, RemoteProvider};
+use broker::{self, api::remote::git, config};
 
-#[test]
-fn clone_public_repo_with_no_auth() {
-    let endpoint = remote::Remote::try_from(String::from("https://github.com/fossas/one")).unwrap();
+#[tokio::test]
+async fn update_clones_on_public_repo_with_no_auth() {
+    let conf = load_config!(
+        "testdata/config/fossa-one-http-no-auth.yml",
+        "testdata/database/empty.sqlite"
+    )
+    .await;
 
-    let clone_dir = tempdir().unwrap();
-    let clone_path = clone_dir.path().display().to_string();
-    let repo = git::repository::Repository {
-        directory: PathBuf::from(clone_path.clone()),
-        transport: git::transport::Transport::Http {
-            endpoint,
-            auth: None,
-        },
-    };
-    let res = repo.clone().unwrap();
-    assert_eq!(PathBuf::from(clone_path.clone()), res);
-    let mut paths: Vec<String> = fs::read_dir(clone_path)
-        .unwrap()
-        .map(|file| String::from(file.unwrap().path().file_name().unwrap().to_str().unwrap()))
-        .collect();
-    paths.sort();
-    let mut expected = vec![String::from("LICENSE"), String::from(".git")];
-    expected.sort();
-    assert_eq!(expected, paths);
+    let mut integrations = conf.integrations().as_ref().iter();
+    let integration = integrations.next().unwrap();
+    let tmpdir = tempdir().unwrap();
+    let path = PathBuf::from(tmpdir.path());
+    let res = git::repository::Repository::update_clones(path, integration).unwrap();
+    let mut master_path = PathBuf::from(tmpdir.path());
+    master_path.push(String::from("master"));
+    let master_res = res
+        .into_iter()
+        .find(|p| p.as_path() == master_path.as_path())
+        .expect("no master path found");
+    assert_eq!(master_res, master_path);
 }
 
-#[test]
-fn clone_private_repo_with_no_auth() {
-    let endpoint = remote::Remote::try_from(String::from("git@github.com:fossas/basis")).unwrap();
+#[tokio::test]
+async fn clone_private_repo_with_no_auth() {
+    let conf = load_config!(
+        "testdata/config/private-repo-http-no-auth.yml",
+        "testdata/database/empty.sqlite"
+    )
+    .await;
 
-    let clone_dir = tempdir().unwrap();
-    let clone_path = clone_dir.path().display().to_string();
-    let repo = git::repository::Repository {
-        directory: PathBuf::from(clone_path),
-        transport: git::transport::Transport::Ssh {
-            endpoint,
-            auth: None,
-        },
-    };
-
-    let context = String::from("cloning private repo with bad auth");
-    assert_error_stack_snapshot!(
-        &context,
-        repo.clone()
-            .expect_err("Could not read from remote repository")
-    );
+    let mut integrations = conf.integrations().as_ref().iter();
+    let integration = integrations.next().unwrap();
+    let tmpdir = tempdir().unwrap();
+    let path = PathBuf::from(tmpdir.path());
+    let res = git::repository::Repository::update_clones(path, integration);
+    assert!(res.is_err());
 }
