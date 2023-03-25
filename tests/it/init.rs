616c24bdac93d650f560bcfa042e4f8cec24cc39
spatten <scott@fossa.com>
2023-03-24 17:44:57 -0700

    integration tests for broker::subcommand::init

---
+++ tests/it/init.rs
@@ -0,0 +1,39 @@
+use std::{fs, path::PathBuf};
+
+#[tokio::test]
+async fn on_empty_dir_creates_config_and_example() {
+    let tmpdir = tempfile::tempdir().unwrap();
+    let tmpdir = PathBuf::from(tmpdir.path());
+    broker::subcommand::init::main(&tmpdir).expect("should init");
+
+    let config_file_path = tmpdir.join("config.yml");
+    assert!(config_file_path.try_exists().unwrap_or(false));
+    let example_file_path = tmpdir.join("config.example.yml");
+    assert!(example_file_path.try_exists().unwrap_or(false));
+}
+
+#[tokio::test]
+async fn when_config_files_exist_only_overwrites_example() {
+    // setup: write config.yml and config.example.yml to the tempdir
+    // init should overwrite config.example.yml but not config.y
+    let tmpdir = tempfile::tempdir().unwrap();
+    let tmpdir = PathBuf::from(tmpdir.path());
+    let config_file_path = tmpdir.join("config.yml");
+    let example_file_path = tmpdir.join("config.example.yml");
+
+    fs::write(&config_file_path, "hello").expect("should write config file");
+    fs::write(&example_file_path, "hello").expect("should write config.example file");
+    broker::subcommand::init::main(&tmpdir).expect("should init");
+
+    assert!(config_file_path.try_exists().unwrap_or(false));
+    assert_eq!(
+        fs::read_to_string(&config_file_path).expect("should read config file"),
+        "hello"
+    );
+
+    assert!(example_file_path.try_exists().unwrap_or(false));
+    assert_ne!(
+        fs::read_to_string(&example_file_path).expect("should read config.example file"),
+        "hello"
+    );
+}
