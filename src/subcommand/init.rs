4e3c91f39349c3c4b94802e38cee59bce087a4ae
Scott Patten <scott@fossa.com>
2023-03-27 14:38:13 -0700

    Update src/subcommand/init.rs
    Co-authored-by: Jessica Black <me@jessica.black>

--- src/subcommand/init.rs
+++ src/subcommand/init.rs
@@ -64,7 +64,7 @@ pub fn main(data_root: &PathBuf) -> Result<(), Error> {
             config = data_root.join("config.yml").display(),
             example_config = data_root.join("config.example.yml").display(),
         };
-        println!("{}", output);
+        println!("{output}");
     }
     Ok(())
 }
