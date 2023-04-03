a770293eeddac60d6e76420ce22cebf322824e67
spatten <scott@fossa.com>
2023-04-03 13:27:55 -0700

    move a bunch of stuff into the Error enum

--- src/subcommand/fix.rs
+++ src/subcommand/fix.rs
@@ -10,7 +10,10 @@ use crate::{
     api::{
         http,
         remote::{
-            git::{repository, transport},
+            git::{
+                repository,
+                transport::{self, Transport},
+            },
             Integration, Protocol, Remote,
         },
         ssh,
@@ -49,6 +52,148 @@ pub enum Error {
         /// The path on Fossa
         path: String,
     },
+}
+
+impl Error {
+    fn fix_explanation(&self) -> String {
+        match self {
+            Error::CheckIntegration { remote, msg, .. } => {
+                format!("❌ {}\n\n{}", remote.to_string().red(), msg)
+            }
+            Error::CheckFossaGet { msg } => {
+                format!("❌ {} {}", "Error checking connection to FOSSA:".red(), msg)
+            }
+            Error::CreateFullFossaUrl { remote, path } => {
+                format!(
+                    "❌ Creating a full URL from your remote of {} and path = {}",
+                    remote, path
+                )
+            }
+        }
+    }
+
+    fn integration_error(
+        remote: &Remote,
+        transport: &Transport,
+        err: Report<repository::Error>,
+    ) -> Self {
+        let msg = formatdoc!(
+            "
+        We encountered an error while trying to connect to your git remote at {}.\n\n{}\n\nFull error message from git:\n\n{}\n\n",
+            remote,
+            Self::protocol_connection_explanation(transport),
+            err.to_string(),
+        );
+        Error::CheckIntegration {
+            remote: remote.clone(),
+            error: err.to_string(),
+            msg,
+        }
+    }
+
+    fn protocol_connection_explanation(transport: &transport::Transport) -> String {
+        let shared_instructions = "We were unable to connect to this repository. Please make sure that the authentication info and the remote are set correctly in your config.yml file.";
+        let base64_command = r#"echo -n "<username>:<password>" | base64"#;
+        let specific_instructions = match transport {
+            transport::Transport::Ssh {
+                auth: ssh::Auth::KeyFile(key_path),
+                endpoint,
+            } => {
+                let key_path = key_path.to_string_lossy();
+                let command = format!(
+                r#"GIT_SSH_COMMAND="ssh -i {} -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -F /dev/null" git ls-remote {}"#,
+                key_path, endpoint
+            ).green();
+                formatdoc!(
+                "You are using SSH keyfile authentication for this remote. This connects to your repository by setting the `GIT_SSH_COMMAND` environment variable with the path to the ssh key that you provided in your config file. Please make sure you can run the following command to verify the connection:
+
+                {}", command
+            )
+            }
+            transport::Transport::Ssh {
+                auth: ssh::Auth::KeyValue(_),
+                endpoint,
+            } => {
+                let command = format!(
+                r#"GIT_SSH_COMMAND="ssh -i <path with ssh key> -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -F /dev/null" git ls-remote {}"#,
+                endpoint
+            ).green();
+                formatdoc!(
+                "You are using SSH key authentication for this remote. This method of authentication writes the SSH key that you provided in your config file to a temporary file, and then connects to your repository by setting the `GIT_SSH_COMMAND` environment variable with the path to the temporary file. To debug this, please write the ssh key to a file and then make sure you can run the following command to verify the connection.
+
+                The path with the ssh key in it must have permissions of 0x660 on Linux and MacOS.
+
+                {}", command
+            )
+            }
+            transport::Transport::Http {
+                auth: Some(http::Auth::Basic { .. }),
+                endpoint,
+            } => {
+                let command = format!(
+                r#"git -c "http.extraHeader=Authorization: Basic <base64 encoded username and password>" {}"#,
+                endpoint
+            )
+            .green();
+
+                formatdoc!(
+                    r#"You are using HTTP basic authentication for this remote. This method of authentication encodes the username and password as a base64 string and then passes that to git using the "http.extraHeader" parameter. To debug this, please make sure that the following commands work.
+
+                You generate the base64 encoded username and password by joining them with a ":" and then base64 encoding them. If your username was "pat" and your password was "password123", then you would base64 encode "pat:password123". For example, you can use a command like this:
+
+                {}
+
+                Once you have the base64 encoded username and password, use them in a command like this:
+
+                {}"#,
+                    base64_command,
+                    command
+                )
+            }
+            transport::Transport::Http {
+                auth: Some(http::Auth::Header { .. }),
+                endpoint,
+            } => {
+                let command = format!(
+                    r#"git -c "http.extraHeader=<your header>" ls-remote {}"#,
+                    endpoint
+                )
+                .green();
+                formatdoc!(
+                    r#"You are using HTTP header authentication for this remote. This method of authentication passes the header that you have provided in your config file to git using the "http.extraHeader" parameter. To debug this, please make sure the following command works, making sure to substitute the header from your config file into the right spot:
+
+                {}
+
+                You generate the header by making a string that looks like this:
+
+                Authorization: Basic <base64 encoded username:password>
+
+                If your username was "pat" and your password was "password123", then you would base64 encode "pat:password123". For example, you can use a command like this:
+
+                {}
+
+                The username you use depends on the git hosting platform you are authenticating to. For details on this, please see the `config.example.yml` file in your broker config directory. You can re-generate this file at any time by running `broker init`.
+                "#,
+                    command,
+                    base64_command
+                )
+            }
+            transport::Transport::Http {
+                auth: None,
+                endpoint,
+            } => {
+                let command = format!("git ls-remote {}", endpoint).green();
+                formatdoc!(
+                    r#"You are using http transport with no authentication for this integration. To debug this, please make sure that the following command works:
+
+                {}"#,
+                    command
+                )
+            }
+        };
+
+        format!("{}\n\n{}", shared_instructions, specific_instructions)
+    }
 }
 
 /// Similar to [`AppContext`], but scoped for this subcommand.
@@ -93,20 +238,7 @@ fn print_errors(msg: String, errors: Vec<Error>) {
     if !errors.is_empty() {
         println!("\n{}\n", msg,);
         for err in errors {
-            match err {
-                Error::CheckIntegration { remote, msg, .. } => {
-                    println!("❌ {}\n\n{}", remote.to_string().red(), msg);
-                }
-                Error::CheckFossaGet { msg } => {
-                    println!("❌ {} {}", "Error checking connection to FOSSA:".red(), msg);
-                }
-                Error::CreateFullFossaUrl { remote, path } => {
-                    println!(
-                        "❌ Creating a full URL from your remote of {} and path = {}",
-                        remote, path
-                    )
-                }
-            }
+            println!("{}", err.fix_explanation());
         }
     }
 }
@@ -140,127 +272,9 @@ async fn check_integrations(ctx: &CmdContext) -> Vec<Error> {
 #[tracing::instrument]
 async fn check_integration(integration: &Integration) -> Result<(), Error> {
     let Protocol::Git(transport) = integration.protocol();
-    repository::ls_remote(transport).or_else(|err| {
-        let remote = integration.remote().clone();
-        let msg = formatdoc!(
-            "
-        We encountered an error while trying to connect to your git remote at {}.\n\n{}\n\nFull error message from git:\n\n{}\n\n",
-            remote,
-            protocol_connection_explanation(transport),
-            err.to_string(),
-        );
-        Error::CheckIntegration {
-            remote,
-            error: err.to_string(),
-            msg,
-        }
-        .wrap_err()
-    })?;
+    repository::ls_remote(transport)
+        .or_else(|err| Error::integration_error(integration.remote(), transport, err).wrap_err())?;
     Ok(())
-}
-
-fn protocol_connection_explanation(transport: &transport::Transport) -> String {
-    let shared_instructions = "We were unable to connect to this repository. Please make sure that the authentication info and the remote are set correctly in your config.yml file.";
-    let base64_command = r#"echo -n "<username>:<password>" | base64"#;
-    let specific_instructions = match transport {
-        transport::Transport::Ssh {
-            auth: ssh::Auth::KeyFile(key_path),
-            endpoint,
-        } => {
-            let key_path = key_path.to_string_lossy();
-            let command = format!(
-                r#"GIT_SSH_COMMAND="ssh -i {} -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -F /dev/null" git ls-remote {}"#,
-                key_path, endpoint
-            ).green();
-            formatdoc!(
-                "You are using SSH keyfile authentication for this remote. This connects to your repository by setting the `GIT_SSH_COMMAND` environment variable with the path to the ssh key that you provided in your config file. Please make sure you can run the following command to verify the connection:
-
-                {}", command
-            )
-        }
-        transport::Transport::Ssh {
-            auth: ssh::Auth::KeyValue(_),
-            endpoint,
-        } => {
-            let command = format!(
-                r#"GIT_SSH_COMMAND="ssh -i <path with ssh key> -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -F /dev/null" git ls-remote {}"#,
-                endpoint
-            ).green();
-            formatdoc!(
-                "You are using SSH key authentication for this remote. This method of authentication writes the SSH key that you provided in your config file to a temporary file, and then connects to your repository by setting the `GIT_SSH_COMMAND` environment variable with the path to the temporary file. To debug this, please write the ssh key to a file and then make sure you can run the following command to verify the connection.
-
-                The path with the ssh key in it must have permissions of 0x660 on Linux and MacOS.
-
-                {}", command
-            )
-        }
-        transport::Transport::Http {
-            auth: Some(http::Auth::Basic { .. }),
-            endpoint,
-        } => {
-            let command = format!(
-                r#"git -c "http.extraHeader=Authorization: Basic <base64 encoded username and password>" {}"#,
-                endpoint
-            )
-            .green();
-
-            formatdoc!(
-                r#"You are using HTTP basic authentication for this remote. This method of authentication encodes the username and password as a base64 string and then passes that to git using the "http.extraHeader" parameter. To debug this, please make sure that the following commands work.
-
-                You generate the base64 encoded username and password by joining them with a ":" and then base64 encoding them. If your username was "pat" and your password was "password123", then you would base64 encode "pat:password123". For example, you can use a command like this:
-
-                {}
-
-                Once you have the base64 encoded username and password, use them in a command like this:
-
-                {}"#,
-                base64_command,
-                command
-            )
-        }
-        transport::Transport::Http {
-            auth: Some(http::Auth::Header { .. }),
-            endpoint,
-        } => {
-            let command = format!(
-                r#"git -c "http.extraHeader=<your header>" ls-remote {}"#,
-                endpoint
-            )
-            .green();
-            formatdoc!(
-                r#"You are using HTTP header authentication for this remote. This method of authentication passes the header that you have provided in your config file to git using the "http.extraHeader" parameter. To debug this, please make sure the following command works, making sure to substitute the header from your config file into the right spot:
-
-                {}
-
-                You generate the header by making a string that looks like this:
-
-                Authorization: Basic <base64 encoded username:password>
-
-                If your username was "pat" and your password was "password123", then you would base64 encode "pat:password123". For example, you can use a command like this:
-
-                {}
-
-                The username you use depends on the git hosting platform you are authenticating to. For details on this, please see the `config.example.yml` file in your broker config directory. You can re-generate this file at any time by running `broker init`.
-                "#,
-                command,
-                base64_command
-            )
-        }
-        transport::Transport::Http {
-            auth: None,
-            endpoint,
-        } => {
-            let command = format!("git ls-remote {}", endpoint).green();
-            formatdoc!(
-                r#"You are using http transport with no authentication for this integration. To debug this, please make sure that the following command works:
-
-                {}"#,
-                command
-            )
-        }
-    };
-
-    format!("{}\n\n{}", shared_instructions, specific_instructions)
 }
 
 #[tracing::instrument(skip(ctx))]
