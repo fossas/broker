53ab351511d248bc3884c8b5c33307e7ea068741
spatten <scott@fossa.com>
2023-04-06 13:19:24 -0700

    remove colour in snapshot

--- tests/it/fix.rs
+++ tests/it/fix.rs
@@ -1,33 +1,32 @@
-use crate::helper::load_config;
+use crate::helper::{load_config, set_snapshot_vars};
 use broker::subcommand::fix::Logger;
 use insta::assert_debug_snapshot;
 
 /// A logger that prints to stdout and also keeps track of what has been logged so that we can test it
 struct TestLogger {
-    output: String,
+    output: Vec<String>,
 }
 
 impl TestLogger {
     fn output(&self) -> String {
-        self.output.clone()
+        self.output.join("")
     }
 
     fn new() -> Self {
-        TestLogger {
-            output: "".to_string(),
-        }
+        TestLogger { output: vec![] }
     }
 }
 
 impl Logger for TestLogger {
     fn log(&mut self, content: &str) {
         println!("{content}");
-        self.output.push_str(content);
+        self.output.push(content.to_string());
     }
 }
 
 #[tokio::test]
 async fn with_http_no_auth_integration() {
+    set_snapshot_vars!();
     let (_, conf) = load_config!(
         "testdata/config/basic-http-no-auth.yml",
         "testdata/database/empty.sqlite"
@@ -38,5 +37,5 @@ async fn with_http_no_auth_integration() {
     broker::subcommand::fix::main(&conf, &mut logger)
         .await
         .expect("should run fix");
-    assert_debug_snapshot!(logger.output().as_str());
+    assert_debug_snapshot!(logger.output());
 }
